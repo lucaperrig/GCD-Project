ce6332cc207368d2818e0b282cc088ff61d36ded
lucaperrig <luca.perrig@protonmail.com>
2019-07-24 18:53:02 +0200

    Code

---
+++ run_analysis.R
@@ -0,0 +1,40 @@
+library(dplyr)
+
+#Read text files train
+setwd("/Users/lucaperrig/switchdrive/Divers/Cours/DataScience/DataScienceR-JH/3-GettingAndCleaningData/Week4/UCI HAR Dataset/train")
+Xtrain <- read.table("X_train.txt")
+ytrain <- read.table("y_train.txt")
+subtrain <- read.table("subject_train.txt")
+
+#Read text files test
+setwd("/Users/lucaperrig/switchdrive/Divers/Cours/DataScience/DataScienceR-JH/3-GettingAndCleaningData/Week4/UCI HAR Dataset/test")
+Xtest <- read.table("X_test.txt")
+ytest <- read.table("y_test.txt")
+subtest <- read.table("subject_test.txt")
+
+#Merge files
+train <- cbind(ytrain, subtrain, Xtrain)
+test <- cbind(ytest, subtest, Xtest)
+df <- rbind(train, test)
+
+#Convert to tbl
+colnames(df)[1] <- "labels"
+colnames(df)[2] <- "subject"
+tbl <- tbl_df(df)
+
+#Activity names
+tbl$labels <- factor(tbl$labels, levels = c(1:6), labels = c("WALKING", "WALKING_UPSTAIRS", "WALKING_DOWNSTAIRS", "SITTING", "STANDING", "LAYING"))
+
+#Variables names
+setwd("/Users/lucaperrig/switchdrive/Divers/Cours/DataScience/DataScienceR-JH/3-GettingAndCleaningData/Week4/UCI HAR Dataset")
+features <- read.table("features.txt", colClasses = c("numeric", "character"))
+colnames(tbl)[3:dim(tbl)[2]] <- features[,2]
+
+#Select means and stds
+ids <- c(1, 2, (grep(pattern = 'mean\\(|std\\(', x = features[,2])+2))
+tbl <- tbl[,ids]
+
+#Averages per subject and activity
+avgs <- group_by(tbl, labels, subject) %>%
+  summarise_at(vars(3:68), mean)
+
